0e326389fc9cf95b247a0cf5524aeabc7fe5cc6a
MohamedKhaled58 <m.khaled64.mmk@gmail.com>
2026-03-12 01:29:16 +0200

    feat: Add core engine class and timing service for main loop and frame rate management.

--- LongXi/LXEngine/Src/Timing/TimingService.h
+++ LongXi/LXEngine/Src/Timing/TimingService.h
@@ -9,9 +9,11 @@ namespace LongXi
 struct TimingSnapshot
 {
     uint64_t FrameIndex = 0;
+    double UnscaledDeltaTimeSeconds = 0.0;
     double DeltaTimeSeconds = 0.0;
     double FrameTimeSeconds = 0.0;
     double TotalTimeSeconds = 0.0;
+    bool bDeltaTimeClamped = false;
 };
 
 class TimingService
@@ -25,6 +27,15 @@ public:
     bool IsInitialized() const;
     bool IsFrameActive() const;
 
+    void SetFrameLimiterEnabled(bool enabled);
+    bool IsFrameLimiterEnabled() const;
+
+    void SetFrameRateLimit(double targetFramesPerSecond);
+    double GetFrameRateLimit() const;
+
+    void SetMaxDeltaTime(double maxDeltaSeconds);
+    double GetMaxDeltaTime() const;
+
     void BeginFrame();
     void EndFrame();
 
@@ -34,9 +45,17 @@ private:
     bool m_Initialized = false;
     bool m_FrameActive = false;
     bool m_HasPreviousFrameTimestamp = false;
+    bool m_FrameLimiterEnabled = true;
 
     Clock::time_point m_PreviousFrameTimestamp = {};
     Clock::time_point m_CurrentFrameStartTimestamp = {};
+
+    double m_TargetFramesPerSecond = 60.0;
+    double m_TargetFrameDurationSeconds = 1.0 / 60.0;
+    double m_MaxDeltaSeconds = 0.100;
+    double m_LargeGapWarningThresholdSeconds = 0.250;
+
+    void EnforceFrameRateLimit();
 
     TimingSnapshot m_Snapshot = {};
 };
